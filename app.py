9a9e56f1a28c1af68b06614eb0cd0c4e36ca8dff
epicoderr <nikolasrbinoski6@gmail.com>
2025-03-30 04:46:35 +0300

    add new movie form

--- app.py
+++ app.py
@@ -14,6 +14,22 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+    review = request.form["review"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO items (title, description, review, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [title, description, review, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -43,16 +59,24 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
+            print("DEBUG SESSION:", session)  # Remove this in production
             return redirect("/")
         else:
             return "VIRHE: väärä tunnus tai salasana"
 
 @app.route("/logout")
 def logout():
-    del session["username"]
+    if "user_id" in session:
+        del session["user_id"]
+    if "username" in session:
+        del session["username"]
+
     return redirect("/")
